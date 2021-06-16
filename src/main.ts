e1383cc7392e2267298b5096ad6d902b42c5ac38
hy <zihong2020@foxmail.com>
2021-06-16 15:10:29 +0800

    feat(styles): add sass(scss)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-15 21:27:49
- * @LastEditTime: 2021-06-16 11:16:31
+ * @LastEditTime: 2021-06-16 15:08:04
  * @LastEditors: Please set LastEditors
  * @Description: main
  * @FilePath: /yh-admin/src/main.ts
@@ -9,7 +9,7 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
-import "@/styles/index.css";
+import "@/styles/index.scss";
 // 通用字体
 // import 'vfonts/Lato.css'
 // 等宽字体
--- src/stores/indes.ts
+++ src/stores/indes.ts
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 11:17:05
- * @LastEditTime: 2021-06-16 14:35:22
+ * @LastEditTime: 2021-06-16 14:46:42
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /naiveui-admin-template/src/store/indes.ts
  */
 // https://github.com/kiaking/rfcs/blob/vuex-5/active-rfcs/0000-vuex-5.md
 import { createPinia } from "pinia";
-
+// https://pinia.esm.dev/getting-started.html#installation
 export const pinia = createPinia();
