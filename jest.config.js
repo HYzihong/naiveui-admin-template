ca811075e301dcb0238da853574954507becd7aa
hy <zihong2020@foxmail.com>
2021-06-16 11:08:26 +0800

    feat(test/e2e): add cypress

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   testEnvironment: "jsdom",
   transform: {
-    //  用 `vue-jest` 处理 `*.vue` 文件
     "^.+\\.vue$": "vue-jest",
-    "^.+\\.jsx?$": "babel-jest", // Adding this line solved the issue
+    "^.+\\.jsx?$": "babel-jest",
     "^.+\\.tsx?$": "ts-jest",
   },
   testMatch: ["**/tests/unit/**/*.[jt]s?(x)"],
-  // 告诉 Jest 处理 `*.vue` 文件
   moduleFileExtensions: ["vue", "js", "json", "jsx", "ts", "tsx", "node"],
 };
---
+++ tests/e2e/integration/first.spec.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-16 10:57:27
+ * @LastEditTime: 2021-06-16 11:00:09
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /naiveui-admin-template/test/e2e/integration/First.spec.ts
+ */
+describe("First", () => {
+  it("visit", () => {
+    cy.visit("http://localhost:3000/");
+  });
+});
